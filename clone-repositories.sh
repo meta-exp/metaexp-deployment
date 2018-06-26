492ad030333ef4679e92db3e130d747c60f20fbe
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-06-26 18:08:43 +0200

    add repository clone-script

---
+++ clone-repositories.sh
@@ -0,0 +1,9 @@
+########### 32de-python
+git clone https://github.com/KDD-OpenSource/32de-python.git
+
+########### neo4j-graph-algorithms
+git clone -b alpha-dev https://github.com/KDD-OpenSource/neo4j-graph-algorithms.git
+
+########### 32de-UI
+git clone https://github.com/KDD-OpenSource/32de-UI.git
+export REACT_APP_API_HOST=http://localhost:8000/
